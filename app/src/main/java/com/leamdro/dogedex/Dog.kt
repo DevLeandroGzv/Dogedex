ee565e2d5217a42be1d1e8e9efe97c973f2d4154
LeandroGonzalez <leandrogonzales@wposs.com>
2023-05-15 15:29:15 -0500

    Se crean el recyclerview y las demas clases **se crea la clase para leer la lista de perros **se crea la data class dog **se crea la segunda actividad para la lista de perros

---
+++ app/src/main/java/com/leamdro/dogedex/Dog.kt
@@ -0,0 +1,19 @@
+package com.leamdro.dogedex
+
+data class Dog(
+    val id: Long,
+    val index: Int,
+    val name: String,
+    val type: String,
+    val heightFemale: Double,
+    val heightMale: Double,
+    val imageUrl:
+    String,
+    val lifeExpectancy: String,
+    val temperament: String,
+    val WeightFemale: Double,
+    val WeightMale: Double
+) {
+
+
+}
